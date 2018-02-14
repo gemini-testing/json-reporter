e391f9867871d593dc0edeb49a6c479f556713da
dmitriy96 <dmitriy.penetrator@gmail.com>
2018-02-14 09:53:27 +0300

    fix: parse cli and config validation

--- test/lib/config.js
+++ test/lib/config.js
@@ -3,17 +3,56 @@
 const parseConfig = require('../../lib/config');
 
 describe('config', () => {
-    afterEach(() => delete process.env['json_reporter_path']);
-
-    it('should be enabled by default', () => {
-        assert.equal(parseConfig({}).enabled, true);
+    beforeEach(function() {
+        this.oldArgv = process.argv;
     });
 
-    describe('json report file path', () => {
+    afterEach(function() {
+        process.argv = this.oldArgv;
+
+        delete process.env['json_reporter_enabled'];
+        delete process.env['json_reporter_path'];
+    });
+
+    describe('enabled', () => {
+        it('should be enabled by default', () => {
+            assert.isTrue(parseConfig({}).enabled);
+        });
+
+        it('should disable by configuration file', () => {
+            const config = parseConfig({enabled: false});
+
+            assert.isFalse(config.enabled);
+        });
+
+        it('should disable by cli', () => {
+            process.argv = process.argv.concat('--json-reporter-enabled', 'false');
+
+            assert.isFalse(parseConfig({}).enabled);
+        });
+
+        it('should disable by environment variable', () => {
+            process.env['json_reporter_enabled'] = 'false';
+
+            assert.isFalse(parseConfig({}).enabled);
+        });
+    });
+
+    describe('path', () => {
+        it('should have default value', () => {
+            assert.equal(parseConfig({}).path, 'json-reporter.json');
+        });
+
         it('should set from configuration file', () => {
             const config = parseConfig({path: 'config/path/report.json'});
 
             assert.equal(config.path, 'config/path/report.json');
+        });
+
+        it('should set from cli', () => {
+            process.argv = process.argv.concat('--json-reporter-path', 'new/path.json');
+
+            assert.equal(parseConfig({}).path, 'new/path.json');
         });
 
         it('should set from environment variable', () => {
