be22678070a970c767b2f8ffaffb38b807a01a2b
Roman Kuznetsov <kuznetsov.roman.orenburg@gmail.com>
2024-04-22 13:19:40 +0300

    feat: add testplane support

---
+++ plugin.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const Collector = require('./lib/collector');
+const toolCollector = require('./lib/collector/tool/testplane');
+const parseConfig = require('./lib/config');
+
+module.exports = (testplane, opts) => {
+    const config = parseConfig(opts);
+
+    if (!config.enabled) {
+        return;
+    }
+
+    const collector = Collector.create(toolCollector, config);
+
+    testplane.on(testplane.events.TEST_PASS, (data) => collector.addSuccess(data));
+
+    testplane.on(testplane.events.TEST_FAIL, (data) => collector.addFail(data));
+
+    testplane.on(testplane.events.TEST_PENDING, (data) => collector.addSkipped(data));
+
+    testplane.on(testplane.events.RETRY, (data) => collector.addRetry(data));
+
+    testplane.on(testplane.events.ERROR, (err, data) => data && collector.addError(data));
+
+    testplane.on(testplane.events.RUNNER_END, () => collector.saveFile());
+};
